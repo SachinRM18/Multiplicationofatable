76ba822faa2f157b01ed58f2754e6696a7c56585
Sachin Mulagund 18 <130238531+SachinRM18@users.noreply.github.com>
2023-04-08 22:34:27 +0530

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   int i, number;
+   printf("Enter a number for its multiplication table: ");
+   scanf("%d", &number);
+   for(i=1; i<=10; i++){
+      printf("%d * %d = %d\n", number, i, (number*i));
+   }
+   return 0;
+}
